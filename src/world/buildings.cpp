6b4dbaa8b3b718b25632368fe031de2f9212dcab
Andreas Wegscheider <a.wegscheider7141@gmail.com>
2024-08-31 22:39:32 +0200

    fix crash when button is clicked outside of game area

--- src/world/buildings.cpp
+++ src/world/buildings.cpp
@@ -12,12 +12,14 @@ void BuildingManager::place_building(BuildingKind kind, ChunkMut &chunk, int x, 
     chunk.m_world_mut->m_buildings.push_back({ kind, { x, y } });
 }
 
-void BuildingManager::draw(Chunk const &chunk, Game const& game) const
+void BuildingManager::draw(Chunk const &chunk, Game const &game) const
 {
-    if (game.state == GameState::Building && chunk.contains_position(game.mouse_over.value())) {
+    if (game.state == GameState::Building && game.mouse_over.has_value()) {
         CellPosition mouse_over = game.mouse_over.value();
-        auto model = m_models[(size_t)game.buildin_kind];
-        DrawModel(model, {(float)mouse_over.x, 0.0f, (float)mouse_over.y}, 1.0f, {255,255,255,255});
+        if (chunk.contains_position(mouse_over)) {
+            auto model = m_models[(size_t)game.buildin_kind];
+            DrawModel(model, { (float)mouse_over.x, 0.0f, (float)mouse_over.y }, 1.0f, { 255, 255, 255, 255 });
+        }
     }
     for (auto const &building : chunk.m_world->m_buildings) {
         if (building.survival_kind == BuildingSurvivalKind::Alive && !chunk.contains_position(building.position)) {
